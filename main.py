822280664c32da5465dd5dd937f948249c41055d
Alina Burca <burcaalina348@gmail.com>
2024-12-18 09:51:14 +0200

    create command delete-song

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from song_storage_tool import add_song
+from song_storage_tool import SongStorage
 if __name__ == "__main__":
 
-   song_id = add_song("D:\Downloads\Bohemiam_Rhapsody.mp3", "Queen", "Bohemian Rhapsody", "1975-10-31", ["rock", "classic"])
-   # while True:
-   #     print("\n--- SongStorage Menu ---")
-   #     print("1. Adaugă melodie")
-   #     print("2. Caută melodii")
-   #     print("3. Creează arhivă")
-   #     print("4. Șterge melodie")
-   #     print("5. Ieșire")
-   #
-   #     choice = input("Alege o opțiune: ")
-   #
-   #     if choice == "1":
-   #         # Adaugă melodie1
-   #         file_path = input("Calea către fișierul melodiei: ")
-   #         artist = input("Artist: ")
-   #         song_name = input("Numele melodiei: ")
-   #         release_date = input("Data apariției (YYYY-MM-DD): ")
-   #         tags = input("Lista de tag-uri (separate prin virgulă): ").split(',')
-   #
-   #         song_id = add_song(file_path, artist, song_name, release_date, tags)
-   #         print(f"ID-ul melodiei adăugate: {song_id}")
+   song_tool = SongStorage()
+   while True:
+       print("\n--- SongStorage Menu ---")
+       print("1. Add song")
+       print("2. Delete song")
+       print("3. Modify song data")
+       print("4. Create Savelist")
+       print("5. Search song")
+       print("6. Play song")
+       print("7. Exit")
+
+       choice = input("Choose an option: ")
+
+       if choice == "1":
+
+           song_path = input("Song path: ")
+           artist = input("Artist: ")
+           song_name = input("Song name: ")
+           release_date = input("Release data (YYYY-MM-DD): ")
+           tags = input("Tag list (comma separated): ").split(',')
+
+           song_id = song_tool.add_song(song_path, artist, song_name, release_date, tags)
+           print(f"ID of added song: {song_id}")
+
+       elif choice == "2":
+              song_id = input("Song ID: ")
+              song_tool.delete_song(song_id)
+       elif choice == "7":
+           break
+
--- song_storage_tool.py
+++ song_storage_tool.py
@@ -13,33 +13,64 @@ DB_CONFIG = {
 
 STORAGE_DIR = "./Storage"
 
-def connect_db():
-    return psycopg2.connect(**DB_CONFIG)
+class SongStorage:
+    def __init__(self):
+        self.conn=psycopg2.connect(**DB_CONFIG)
+        self.cursor=self.conn.cursor()
+        if not os.path.exists(STORAGE_DIR):
+            os.makedirs(STORAGE_DIR)
 
-def add_song(file_path, artist, song_name, release_date, tags):
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
 
-        file_name = os.path.basename(file_path)
-        print("basename: ", file_name)
-        destination = os.path.join(STORAGE_DIR, file_name)
-        print("dest: ", destination)
-        shutil.copy(file_path, destination)
+    def add_song(self, song_path, artist, song_name, release_date, tags):
 
-        cursor.execute("""
-            INSERT INTO songs (file_name, artist, song_name, release_date, tags)
-            VALUES (%s, %s, %s, %s, %s) RETURNING id;
-        """, (file_name, artist, song_name, release_date, tags))
-        song_id = cursor.fetchone()[0]
+        if not os.path.exists(song_path):
+            print("Error: File does not exist.")
+        try:
+            file_name = os.path.basename(song_path)
+            print("basename: ", file_name)
+            destination = os.path.join(STORAGE_DIR, file_name)
+            print("dest: ", destination)
+            shutil.copy(song_path, destination)
 
-        conn.commit()
-        print(f"The song was added with ID: {song_id}")
-        return song_id
+            self.cursor.execute("""
+                INSERT INTO songs (file_name, artist, song_name, release_date, tags)
+                VALUES (%s, %s, %s, %s, %s) RETURNING id;
+            """, (file_name, artist, song_name, release_date, tags))
+            song_id = self.cursor.fetchone()[0]
 
-    except Exception as e:
-        print(f"Error adding the song: {e}")
-    finally:
-        cursor.close()
-        conn.close()
+            self.conn.commit()
+            print(f"The song was added with ID: {song_id}")
+            return song_id
+
+        except Exception as e:
+            print(f"Error adding the song: {e}")
+
+
+    def delete_song (self, song_id):
+        try:
+            self.cursor.execute("SELECT file_name FROM songs WHERE id=%s", (song_id,))
+            result=self.cursor.fetchone()
+            if result:
+                song=result[0]
+                print("song: ", song)
+                if os.path.exists(os.path.join(STORAGE_DIR, song)):
+                    os.remove(os.path.join(STORAGE_DIR, song))
+                else:
+                    print("Error: File not found.")
+                self.cursor.execute("DELETE FROM songs WHERE id=%s", (song_id,))
+                self.conn.commit()
+                print("Song deleted successfully.")
+            else:
+                print("Song id was not found.")
+        except Exception as e:
+            print(f"Error deleting the song : {e}")
+
+    def modify_data(self, song_id, **args):
+        try:
+            for key, value in args.items():
+                self.cursor.execute(f"UPDATE songs SET {key}=%s WHERE id=%s", (value, song_id))
+            self.conn.commit()
+            print("Data modified successfully.")
+        except Exception as e:
+            print(f"Error modifying the data: {e}")
 
